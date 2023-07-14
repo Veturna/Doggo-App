f5d1b31ea9caaeed9c731ff4044665e29c20c922
Veturna <dpullo@outlook.com>
2023-07-14 12:49:18 +0200

    correct the css for main page

--- doggo-app/src/components/Information.js
+++ doggo-app/src/components/Information.js
@@ -7,12 +7,12 @@ const Information = () => {
         <div className="information">
                     <img className="dogPic"src="/PinkDogs.svg" alt="dog-on-pink-background"/>
                 <div className= "informationElements">
-                <div className="content is-normal">
+                <div className="content">
                     <h3>Kochamy pieski różne, śmieszne i nieśmieszne, kochane.
                     Dlatego stworzyliśmy stronę, która jest zbiorem wszystkich istniejących ras.</h3>
                     <h3>Klinknij w link poniżej i przekonaj się jak działa nasza wyszukiwarka.</h3>
                     <h3>A jak nie znasz jeszcze żadnej rasy to poniżej znajdziesz specjalną listę</h3>
-                    <button className="button is-warning is-medium is-responsive is-rounded"><Link to={"/search"}>Znajdź swoją ulubioną rasę</Link></button>
+                    <button><Link to={"/search"}>Znajdź swoją ulubioną rasę</Link></button>
                 </div>
                 </div>
             </div>
